9f291f11b2708284b100ec75c00f38e529937b2c
thsno02 <taihangshan2022@gmail.com>
2022-06-23 15:41:40 +0800

    add pip install

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ conda activate vc
 conda config --set ssl_verify true 
 conda config --set proxy_servers.http http://127.0.0.1:7890
 conda config --set show_channel_urls yes
+pip install -r requirements.txt
 conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
