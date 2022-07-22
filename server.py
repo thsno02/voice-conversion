d04dacc33d592de36a02ff095befd0edaa01154c
thsno02 <taihangshan2022@gmail.com>
2022-07-22 11:00:45 +0800

    add comments and comment print

--- server.py
+++ server.py
@@ -16,26 +16,28 @@ from infer.application import Application
 
 
 def draw_plot():
+    # find the server address
     ip = socket.gethostbyname_ex(socket.gethostname())[-1][1]
     server_address = 'http://' + ip + ':8000'
+    # draw qr code with highest error tolerance
     url = pyqrcode.QRCode(server_address, error='H')
-    url.png('qr_code.png', scale=10)
-    im = Image.open('qr_code.png')
+    url.png('./src/qr_code.png', scale=10)
+    im = Image.open('./src/qr_code.png')
     im = im.convert("RGBA")
-    logo = Image.open('logo.png')
+    logo = Image.open('./src/mlogo.png')
     box = (135, 135, 235, 235)
     im.crop(box)
     region = logo
     region = region.resize((box[2] - box[0], box[3] - box[1]))
     im.paste(region, box)
-    im.show()
+    im.show(title='QR Code')
 
 
 serve = Flask(__name__)
 CORS(serve)
 
 app = Application()
-print('模型已初始化。')
+print('Model has been initialized.')
 print(f'id is {id(app)}')
 
 
@@ -44,24 +46,24 @@ def hello_world():
     action = request.args.get("action")
     if action == "start":
         # app.speaker = 1
-        print('开始设置发音者')
+        # print('开始设置发音者')
         speaker = int(request.args.get("speaker"))
-        print(speaker)
+        # print(speaker)
         if app.verify_speaker(speaker):
             app.speaker = speaker
         else:
             return {"status": 1, "message": "action error!"}
-        print('成功设置发音者')
-        print('开始录音')
+        # print('成功设置发音者')
+        # print('开始录音')
         app.start()
         return {"status": 0, "message": "开始!"}
     elif action == "stop":
         # 停止接收 开始处理 播放
-        print('停止录音')
+        # print('停止录音')
         app.stop()
-        print('开始推理')
+        # print('开始推理')
         app.infer()
-        print('结束推理\n')
+        # print('结束推理\n')
         return {"status": 0, "message": "结束!"}
     else:
         return {"status": 1, "message": "action error!"}
