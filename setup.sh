bddd2014babafbb248c9044165790fed1c22f40a
thsno02 <taihangshan2022@gmail.com>
2022-07-25 11:51:33 +0800

    modify the order

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ conda activate vc
 conda config --set ssl_verify true 
 conda config --set proxy_servers.http http://127.0.0.1:7890
 conda config --set show_channel_urls yes
-pip install -r requirements.txt
+conda config --add channels conda-forge
+# install dependencies
 conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+pip install -r requirements.txt
